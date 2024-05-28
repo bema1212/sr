9dba61d9cf98d2ef490328589dbe3148152d9d6d
bema1212 <bouwmanenergie@outlook.com>
2024-05-28 12:08:12 +0200

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -1,12 +1,10 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
   const { method } = req;
   let { url } = req.query;
 
   // Set CORS headers
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   // Handle OPTIONS preflight request
@@ -17,18 +15,12 @@ export default async function handler(req, res) {
   // Retrieve authorization header from the incoming request
   const authorizationHeader = req.headers['authorization'];
 
-  console.log('Request method:', method);
-  console.log('Request URL:', url);
-  console.log('Authorization header:', authorizationHeader);
-
   switch (method) {
     case "GET":
       try {
-        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        if (!url.includes("http://") && !url.includes("https://")) {
           url = "https://" + url;
         }
-
-        console.log('Fetching URL:', url);
 
         // Set up the headers for the fetch request
         const headers = {};
@@ -41,7 +33,7 @@ export default async function handler(req, res) {
         res.status(200).json({ html });
       } catch (error) {
         console.error("Error fetching HTML:", error);
-        res.status(500).json({ error: "Internal Server Error", details: error.message });
+        res.status(500).json({ error: "Internal Server Error" });
       }
       break;
     default:
