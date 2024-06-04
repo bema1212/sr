ecbb0fbcd619c719073201efb1a058839ab25f1a
bema1212 <bouwmanenergie@outlook.com>
2024-06-04 14:44:03 +0200

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
   const { method } = req;
   let { url } = req.query;
@@ -7,7 +5,7 @@ export default async function handler(req, res) {
   // Set CORS headers
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Credentials","true"); 
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Timestamp");
 
   // Handle OPTIONS preflight request
@@ -48,41 +46,14 @@ export default async function handler(req, res) {
           headers['X-Timestamp'] = timestampHeader;
         }
 
-        // Use fetch to request the URL
-        const response = await fetch(encodeURI(url), {
-          headers,
-          credentials: 'include' // Include credentials to handle cookies like JSESSIONID and lbsticky
-        });
-
-        // Retrieve the JSESSIONID and lbsticky from the response cookies
-        const setCookie = response.headers.raw()['set-cookie'];
-        let jsessionid = null;
-        let lbsticky = null;
-
-        if (setCookie) {
-          const jsessionidCookie = setCookie.find(cookie => cookie.startsWith('JSESSIONID='));
-          const lbstickyCookie = setCookie.find(cookie => cookie.startsWith('lbsticky='));
-          
-          if (jsessionidCookie) {
-            jsessionid = jsessionidCookie.split(';')[0].split('=')[1];
-          }
-          if (lbstickyCookie) {
-            lbsticky = lbstickyCookie.split(';')[0].split('=')[1];
-          }
-        }
-
-        console.log('JSESSIONID:', jsessionid);
-        console.log('LBSticky:', lbsticky);
-
+        const response = await fetch(encodeURI(url), { headers });
         const html = await response.text();
         
         // Parse the HTML string as JSON
         const jsonResponse = JSON.parse(html);
 
-        // Respond with the JSON content and include the JSESSIONID and lbsticky if needed
-        res.setHeader('X-Session-ID', jsessionid); // Set the session ID in the response headers
-        res.setHeader('X-LBSticky', lbsticky); // Set the lbsticky in the response headers
-        res.status(200).json({ jsessionid, lbsticky, ...jsonResponse });
+        // Respond with the JSON content
+        res.status(200).json(jsonResponse);
       } catch (error) {
         console.error("Error fetching JSON:", error);
         res.status(500).json({ error: "Internal Server Error", details: error.message });
